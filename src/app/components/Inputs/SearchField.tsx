ac76d6e4b82a61765bf11165ed28305d10258dd2
Marcelo Pignataro <pignataromd@gmail.com>
2024-11-09 17:43:02 +0000

    Takes svgs from specific files in page. Takes svgs from specific files in SearchField.

--- src/app/components/Inputs/SearchField.tsx
+++ src/app/components/Inputs/SearchField.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ChangeEvent } from "react";
+import Search from "../Icons/search";
 
 export default function SearchField({
 	value,
@@ -22,20 +23,7 @@ export default function SearchField({
 				</label>
 				<div className="relative">
 					<div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-						<svg
-							className="w-5 h-5 text-gray-500 dark:text-gray-400"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-							></path>
-						</svg>
+						<Search />
 					</div>
 					<input
 						type="search"
